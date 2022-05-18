c281df5371f159a6cbd20050f00efdea0779c494
shamb <shamb@DESKTOP-HNGPND9>
2022-05-18 22:52:28 +0530

    Add project files.

---
+++ MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,62 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<mvcEmployeeModel> empList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employees").Result;
+            empList = response.Content.ReadAsAsync <IEnumerable<mvcEmployeeModel>>().Result;
+
+            return View(empList);
+        }
+
+        public ActionResult AddOrEdit(int id=0)
+        {
+
+            if(id==0)
+                return View(new mvcEmployeeModel());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employees/"+id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<mvcEmployeeModel>().Result);
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(mvcEmployeeModel emp)
+        {
+            if(emp.EmployeeID==0)
+            {
+
+            HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employees", emp).Result;
+            TempData["SuccessMessage"] = "Save Successfully";
+            return RedirectToAction("Index");
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Employees/" +emp.EmployeeID,emp).Result;
+                TempData["SuccessMessage"] = "Update Successfully";
+                return RedirectToAction("Index");
+            }
+        
+    }
+      public ActionResult Delete(int id)
+        {
+
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Employees/" + id.ToString()).Result;
+
+            TempData["SuccessMessage"] = "Delete Successfully";
+            return RedirectToAction("Index");
+        }  
+
+    }
+}
---
+++ MVC/GlobalVariables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace MVC
+{
+    public static class GlobalVariables
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+
+         static GlobalVariables()
+        {
+            WebApiClient.BaseAddress = new Uri("http://localhost:2347/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add( new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+    }
+}
